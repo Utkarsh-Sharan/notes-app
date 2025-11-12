08e329a2c237fae8f9c8e19c7b27b5ceeefe9196
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-12 15:50:10 +0530

    setup mongo db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
@@ -7,6 +8,13 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port http://localhost/${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port http://localhost/${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect with MongoDB!", err);
+    process.exit(1);
+  });
