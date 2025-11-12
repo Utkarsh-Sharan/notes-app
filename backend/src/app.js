afa0e66a92aec4e0aa038007346eed503cab3503
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-12 12:18:45 +0530

    initial setup

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+//import routes
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hi from notes app!");
+});
+
+export default app;
