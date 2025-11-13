34748884e0970bde99599a13848fa9e1d7dc6305
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-13 11:13:39 +0530

    updated updateANote and deleteANote controllers

--- backend/src/controllers/notes.controllers.js
+++ backend/src/controllers/notes.controllers.js
@@ -26,12 +26,43 @@ const createANote = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, { newNote }, "Note saved successfully!"));
 });
 
-const updateANote = async (req, res) => {
-  res.status(200).json(new ApiResponse(200, {}, "Note updated successfully!"));
-};
+const updateANote = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+  const updatedNote = await Note.findByIdAndUpdate(
+    req.params.id,
+    {
+      title,
+      description,
+    },
+    { new: true },
+  );
 
-const deleteANote = async (req, res) => {
-  res.status(200).json(new ApiResponse(200, {}, "Note deleted successfully!"));
-};
+  if (!updatedNote) throw new ApiError(404, "Note not found!");
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, { updatedNote }, "Note updated successfully!"));
+});
+
+const deleteANote = asyncHandler(async (req, res) => {
+  const noteToDelete = await Note.findById(req.params.id);
+  const { title, description } = {
+    title: noteToDelete.title,
+    description: noteToDelete.description,
+  };
+  const deletedNote = await Note.findByIdAndDelete(req.params.id);
+
+  if (!deletedNote) throw new ApiError(404, "Note not found!");
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { title, description },
+        "Note deleted successfully!",
+      ),
+    );
+});
 
 export { getAllNotes, createANote, updateANote, deleteANote };
