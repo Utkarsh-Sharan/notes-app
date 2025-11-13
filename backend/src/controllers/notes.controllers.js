56660352be0766ed6b9685447b40e47e9f5c5f98
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-13 10:32:42 +0530

    updated getAllNotes and createANote controllers

--- backend/src/controllers/notes.controllers.js
+++ backend/src/controllers/notes.controllers.js
@@ -1,14 +1,30 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Note } from "../models/note.models.js";
 
-const getAllNotes = async (req, res) => {
-  res.status(200).json(new ApiResponse(200, {}, "Watch all your notes here!"));
-};
+const getAllNotes = asyncHandler(async (req, res) => {
+  const notes = await Note.find();
 
-const createANote = async (req, res) => {
+  if (!notes) throw new ApiError(404, "Notes not found!");
+
   res
+    .status(200)
+    .json(new ApiResponse(200, { notes }, "Notes fetched successfully!"));
+});
+
+const createANote = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+  const newNote = new Note({ title, description });
+
+  if (!newNote) throw new ApiError(400, "Failed to create note!");
+
+  await newNote.save({ validateBeforeSave: false });
+
+  return res
     .status(201)
-    .json(new ApiResponse(201, {}, "New note created successfully!"));
-};
+    .json(new ApiResponse(201, { newNote }, "Note saved successfully!"));
+});
 
 const updateANote = async (req, res) => {
   res.status(200).json(new ApiResponse(200, {}, "Note updated successfully!"));
