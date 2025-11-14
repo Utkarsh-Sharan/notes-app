8d093d0d90d5f15a355f12e04edb0c10369342f8
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-14 11:11:51 +0530

    setup tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,10 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailsPage from "./pages/NoteDetailsPage.jsx";
-import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Click successful!")}>Click</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
