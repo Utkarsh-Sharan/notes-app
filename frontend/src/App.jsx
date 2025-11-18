6fc63db5267dd17bd1cb0de7283967287e635bca
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-18 12:13:08 +0530

    home page bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import HomePage from "./pages/homePage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailsPage from "./pages/NoteDetailsPage.jsx";
 
