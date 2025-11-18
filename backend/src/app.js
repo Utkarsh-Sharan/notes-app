aea73cbd20c8f5914f05266cb01a65e0ae30dc5d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-18 12:02:13 +0530

    prep for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { ApiResponse } from "./utils/ApiResponse.js";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
 //basic configurations
 app.use(express.json({ limit: "16kb" }));
@@ -10,18 +11,28 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 //CORS configuration
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+  );
+}
 
 //import routes
 import notesRoutes from "./routes/notes.routes.js";
 
 app.use("/api/v1/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 export default app;
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api/v1"
+    : "/api/v1";
+
 export const api = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL: BASE_URL,
 });
